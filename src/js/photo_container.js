6c89910487727cf5f716ce0457b48a560e962d2e
Trevor Rapp <trrapp@gmail.com>
2021-09-23 22:41:55 -0600

    adds China photos, gets bable translated version of React to render

--- src/js/photo_container.js
+++ src/js/photo_container.js
@@ -1,30 +1,77 @@
-alert("photo_container.js connected");
+//
+// console.log("entered photo_container.js")
+//
+// class PhotoContainer extends React.Component {
+//   constructor(props) {
+//     super(props);
+//
+//   }
+//
+//   render () {
+//     return (
+//       <div>
+//         <ul>
+//           <li>
+//             <img src="./src/assets/china/111.jpg"/>
+//           </li>
+//         </ul>
+//       </div>
+//
+//     );
+//   }
+// }
+//
+// ReactDOM.render(<PhotoContainer/>, document.getElementById('photo_container'));
 
-ReactDOM.render(JSX, document.getElementById('root'))
+"use strict";
 
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
-class PhotoContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {liked: false};
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+console.log("entered photo_container.js");
+
+var PhotoContainer = /*#__PURE__*/function (_React$Component) {
+  _inherits(PhotoContainer, _React$Component);
+
+  var _super = _createSuper(PhotoContainer);
+
+  function PhotoContainer(props) {
+    _classCallCheck(this, PhotoContainer);
+
+    return _super.call(this, props);
   }
 
-  render () {
-    if (this.state.liked) {
-      return "You liked this"
+  _createClass(PhotoContainer, [{
+    key: "render",
+    value: function render() {
+      return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, /*#__PURE__*/React.createElement("img", {
+        src: "./src/assets/china/111.jpg"
+      }))));
     }
+  }]);
 
-    return (
-      <button onClick={() => this.setState({ liked: true })}>
-        Like
-      </button>
+  return PhotoContainer;
+}(React.Component);
 
-    );
-  }
-}
-
-
-
-
-const domContainer = document.querySelector('#photo_container');
-ReactDOM.render(e(PhotoContainer), domContainer);
+ReactDOM.render( /*#__PURE__*/React.createElement(PhotoContainer, null), document.getElementById('photo_container'));
