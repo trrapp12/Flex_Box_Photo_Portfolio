bda9dc62a59d0f5ed6373d8815dcaa45b73e8cd8
Trevor Rapp <trrapp@gmail.com>
2022-01-24 22:49:00 -0700

    start new js for photos

---
+++ src/js/randomize_photos.js
@@ -0,0 +1,9 @@
+(function({
+  const spainPhotos = [];
+  const cambodiaPhotos = [];
+  const parisMorroccoPhotos = [];
+  const unitedStatePhotos = [];
+  const elSalvadorPhotos = [];
+  const chinaPhotos = [];
+  const spainPhotos = [];
+})()
